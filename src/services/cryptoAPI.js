78a4ed75d045d96ab0116575d87d7066a157ef05
Justin V <justinv4994@gmail.com>
2021-12-02 18:34:09 -0500

    updated Redux-API calls

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -5,7 +5,7 @@ const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
 };
 
-const baseURL = 'https://coinranking1.p.rapidapi.com';
+const baseURL = 'https://coinranking1.p.rapidapi.com' ;
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
@@ -20,32 +20,3 @@ export const cryptoAPI = createApi({
 });
 
 export const { useGetCryptosQuery } = cryptoAPI;
-
-// const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
-
-// export const cryptoApi = createApi({
-//   reducerPath: 'cryptoApi',
-//   baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
-//   endpoints: (builder) => ({
-//     getCryptos: builder.query({
-//       query: (count) => createRequest(`/coins`),
-//     }),
-//     getExchanges: builder.query({
-//       query: () => createRequest('/exchanges'),
-//     }),
-//     getCryptoDetails: builder.query({
-//       query: (coinId) => createRequest(`/coin/${coinId}`),
-//     }),
-//     getCryptoHistory: builder.query({
-//       query: ({ coinId, timeperiod }) =>
-//         createRequest(`coin/${coinId}/history/${timeperiod}`),
-//     }),
-//   }),
-// });
-
-// export const {
-//   useGetCryptosQuery,
-//   useGetCryptoDetailsQuery,
-//   useGetExchangesQuery,
-//   useGetCryptoHistoryQuery,
-// } = cryptoApi;
